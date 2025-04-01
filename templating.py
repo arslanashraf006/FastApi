60bb7a744cd85f2b5b93379f03c0851a54eb9067
arslanashraf006 <arslanashraf7864@gmail.com>
2025-04-01 10:15:57 +0500

    Added : templating -> connecting mongodb database with the fastapi backend

--- templating.py
+++ templating.py
@@ -2,15 +2,22 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import pymongo
 
 app = FastAPI()
+
+#conecting with mongodb
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+database = client["friends"]
+collection = database["best_friend"]
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-def read_item(request: Request):
+@app.get("/{name}", response_class=HTMLResponse)
+def read_item(request: Request, name):
+    collection.insert_one({"name": name})
     return templates.TemplateResponse(
         request=request, name="home.html",
     )
