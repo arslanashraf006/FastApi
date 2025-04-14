f8eb1affe93df5f741688c7d3d9e09923f89a6ee
arslanashraf006 <arslanashraf7864@gmail.com>
2025-04-14 10:56:21 +0500

    Added : Main -> accessing data of backend in templates

--- main.py
+++ main.py
@@ -1,17 +1,55 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import pymongo
 
 app = FastAPI()
 
-@app.get("/hello")
-def home():
-    return "hello world"
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def read_item(request: Request, name):
+    return templates.TemplateResponse(
+        request=request, name="home.html",
+    )
 
 
-@app.get("/about")
-def about():
-    dataobj = {
-        "name": "Ali",
-        "age":  28,
-        "subject":"python"
+@app.get("/about", response_class=HTMLResponse)
+def read_item(request: Request):
+    name = "Ali"
+    age = 15
+    subject = "web development"
+
+    user = {
+        "name": name,
+        "age": age,
+        "subject": subject,
     }
-    return dataobj
+    return templates.TemplateResponse(
+        request=request, name="about.html", context= {"user": user}
+    )
+
+
+
+
+
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# @app.get("/hello")
+# def home():
+#     return "hello world"
+
+
+# @app.get("/about")
+# def about():
+#     dataobj = {
+#         "name": "Ali",
+#         "age":  28,
+#         "subject":"python"
+#     }
+#     return dataobj
