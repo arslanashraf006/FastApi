2def33b0abcae5312cfceb9ca50b79609938438c
arslanashraf006 <arslanashraf7864@gmail.com>
2025-04-15 15:19:26 +0500

    Added : Main -> obtaning data of mongodb in the templates of fastapi

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ import pymongo
 
 app = FastAPI()
 
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+database = client["person"]
+collection = database["employee"]
+
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-def read_item(request: Request, name):
+def read_item(request: Request,):
+
     return templates.TemplateResponse(
         request=request, name="home.html",
     )
@@ -19,18 +25,27 @@ def read_item(request: Request, name):
 
 @app.get("/about", response_class=HTMLResponse)
 def read_item(request: Request):
-    name = "Ali"
-    age = 15
-    subject = "web development"
+    # name = "Ali"
+    # age = 15
+    # subject = "web development"
 
-    user = {
-        "name": name,
-        "age": age,
-        "subject": subject,
-    }
+    # user = {
+    #     "name": name,
+    #     "age": age,
+    #     "subject": subject,
+    # }
+    user = []
+    raw_data = collection.find()
+    for item in raw_data:
+        print(item)
+        user.append(item)
+    print(user)
     return templates.TemplateResponse(
-        request=request, name="about.html", context= {"fruits": ["apple", "mango", "guave", "pineapple", "banana", "grapes"]}
+        request=request, name="about.html", context= {"user":user }
     )
+    # return templates.TemplateResponse(
+    #     request=request, name="about.html", context= {"fruits": ["apple", "mango", "guave", "pineapple", "banana", "grapes"]}
+    # )
 
 
 
